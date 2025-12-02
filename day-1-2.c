f8ecceea8ab2989662df3dabac90c391143a4fec
Filipsys <filipsysak@gmail.com>
2025-12-02 23:39:07 +0100

    Unfinished day 1-2, unfinished day 2-1

---
+++ day-1-2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main() {
+  int fd = open("day-1.input", O_RDONLY);
+  struct stat st;
+  fstat(fd, &st);
+
+  size_t len = st.st_size;
+  void *p = mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+  char *str = (char *) p;
+  close(fd);
+
+  char *pch = strtok(str, "\n");
+  int hitCount = 0;
+  int currentAngle = 50;
+
+  while (pch != NULL) {
+    signed char dir = *pch == 'L' ? -1 : 1; 
+    int angle = atoi(pch + 1);
+
+    currentAngle += angle * dir;
+
+    while (currentAngle >= 100 || currentAngle <= -100) {
+      currentAngle -= currentAngle >= 100 ? 100 : -100;
+      hitCount++;
+    }
+
+    if (currentAngle < 0) currentAngle += 100;
+    if (currentAngle == 0) hitCount++;
+
+    pch = strtok(NULL, "\n");
+  }
+
+  munmap(p, len);
+  printf("Zero hit count: %d\n", hitCount);
+
+  return 0;
+}
---
+++ day-2-1.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main() {
+  int fd = open("day-2.input", O_RDONLY);
+  struct stat st;
+  fstat(fd, &st);
+
+  size_t len = st.st_size;
+  void *p = mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+  char *str = (char *) p;
+
+  close(fd);
+
+  char *saveptr1;
+  char *saveptr2;
+  char *pch = strtok_r(str, ",", &saveptr1);
+
+  unsigned long long int sum = 0;
+  while (pch != NULL) {
+    char *pnums = strtok_r(pch, "-", &saveptr2);
+    unsigned long long int fnum = strtoull(pnums, NULL, 10);
+
+    pnums = strtok_r(NULL, "-", &saveptr2);
+    unsigned long long int snum = strtoull(pnums, NULL, 10);
+
+    printf("%llu to %llu\n", fnum, snum);
+
+    for (int i = fnum; i <= snum; i++) {
+      int strlength = snprintf(NULL, 0, "%d", i);
+      if (strlength % 2 != 0) continue;
+      int halflen = strlength / 2;
+
+      char *strnum = malloc(strlength + 1);
+      snprintf(strnum, strlength + 1, "%d", i);
+
+      char *fpart = malloc(halflen + 1);
+      strncpy(fpart, strnum, halflen);
+      fpart[halflen] = '\0';
+      
+      char *spart = malloc(halflen + 1);
+      strncpy(spart, strnum + halflen, halflen);
+      spart[halflen] = '\0';
+
+      if (strcmp(fpart, spart) == 0) sum += i;
+
+      free(strnum);
+      free(fpart);
+      free(spart);
+    }
+
+    pch = strtok_r(NULL, ",", &saveptr1);
+  }
+
+  munmap(p, len);
+  printf("Sum: %llu\n", sum);
+
+  return 0;
+}
