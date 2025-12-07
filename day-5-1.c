ef443a6613fc23df42920c17fc4a3be633ec02f2
Filipsys <filipsysak@gmail.com>
2025-12-07 18:15:04 +0100

    Completed day 5 task 1

--- day-5-1.c
+++ day-5-1.c
@@ -7,6 +7,11 @@
 #include <string.h>
 #include <stdio.h>
 
+struct tuple {
+  long n1;
+  long n2;
+};
+
 int main() {
   int fd = open("day-5.input", O_RDONLY);
   struct stat st;
@@ -19,7 +24,6 @@ int main() {
 
   int npos = 0;
   for (int i = 0; ; i++) {
-    printf("%02X ", str[i]);
     if (str[i] == '\0') break;
     if (str[i] == '\n' && str[i + 1] == '\n') npos = i;
   }
@@ -28,29 +32,53 @@ int main() {
   char *nums = &str[npos + 2];
   char *saveptr1;
   char *saveptr2;
-  // char *rngs = strtok_r(str, "\n", &saveptr1);
+  char *rngscp = strdup(str);
+  char *rngs = strtok_r(str, "\n", &saveptr1);
   char *pnums = strtok_r(nums, "\n", &saveptr2);
 
-  for (int i = 0; ; i++) {
-    if (str[i] == '\0') break;
-  }
-  // printf("RNGS\n%s\nNUMS\n%s", str, nums);
+  char *saveptr3;
+  rngscp = strtok_r(rngscp, "\n", &saveptr3);
 
-  while (pnums != NULL) {
-    for (int j = 0; ; j++) {
-      // if (rngs[j] == '\0') break;
-
-      // printf("%s\n", rngs);
-      // int start = strtol(strtok(rngs[j], "-"), NULL, 10);
-      // int end = strtol(strtok(NULL, "-"), NULL, 10);
-
-      // printf("Start: %d, end: %d", start, end);
-
-  //     rngs = strtok_r(NULL, "\n", &saveptr2);
+  int tplen = 0;
+  while (rngscp != NULL) {
+    for (int i = 0; ; i++) {
+      if (rngscp[i] == '\0') break;
+      if (rngscp[i] == '-') tplen++; 
     }
-  
-    nums = strtok_r(NULL, "\n", &saveptr1);
+    
+    rngscp = strtok_r(NULL, "\n", &saveptr3);
   }
+
+  struct tuple *tprngs = malloc(tplen * sizeof(struct tuple));
+  char *saveptr4;
+  int tploc = 0;
+  while (rngs != NULL) {
+    long n1 = strtol(strtok_r(rngs, "-", &saveptr4), NULL, 10);
+    long n2 = strtol(strtok_r(NULL, "-", &saveptr4), NULL, 10);
+
+    tprngs[tploc] = (struct tuple) {n1, n2};
+    tploc++;
+
+    rngs = strtok_r(NULL, "\n", &saveptr1);
+  }
+
+  int fresh = 0;
+  while (pnums != NULL) {
+    long num = strtol(pnums, NULL, 10);
+
+    for (int j = 0; j < tplen; j++) {
+      if (num >= tprngs[j].n1 && num <= tprngs[j].n2) {
+        printf("%ld fits between %ld and %ld\n", num, tprngs[j].n1, tprngs[j].n2);
+        fresh++;
+        break;
+      }
+    }
+
+    pnums = strtok_r(NULL, "\n", &saveptr2);
+  }
+
+  free(tprngs);
+  printf("Fresh ingredients: %d\n", fresh);
 
   return 0;
 }
